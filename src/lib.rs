d9370f08b1402d596ab3b698f77f7f5efe586626
Francesco Macagno <fjmacagno1@yahoo.com>
2018-01-28 00:38:49 -0800

    trying another way

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,9 @@ use tokio_core::net::{TcpListener, UdpSocket};
 use tokio_core::reactor::Core;
 
 use std::collections::HashMap;
-use std::{env, io};
+use std::{io};
+use std::thread;
+use std::sync::Mutex;
 
 mod protocol;
 
@@ -22,66 +24,94 @@ pub struct Network {
     num_devices: u32,
     data: HashMap<String, i32>,
     interests: Vec<String>,
-    broadcast_sock: UdpSocket,
-    port: u16
+    broadcast_sock: Option<UdpSocket>,
+    port: u16,
+    servers_started: bool
 }
 
 impl Network {
 
     pub fn new(interests: Vec<String>)-> Network {
 
-        let mut core = Core::new().unwrap();
-        let handle = core.handle();
 
-        let udpaddr = "127.0.0.1:52300".parse().unwrap();
-        let tcpaddr = "127.0.0.1:0".parse().unwrap();
 
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //UDP
-        let broadcast_sock: UdpSocket = match UdpSocket::bind(&udpaddr, &handle) {
-            Ok(sock) => sock,
-            Err(error) => panic!("Couldn't listen for UDP! {}", error)
-        };
 
-        broadcast_sock.set_broadcast(true).unwrap();
-
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // TCP
-        let tcp_listener = match TcpListener::bind(&tcpaddr, &handle) {
-            Ok(lst) => lst,
-            Err(error) => panic!("Couldn't listen for TCP! {}", error)
-        };
-
-        let port = tcp_listener.local_addr().unwrap().port();
-        println!("TCP Server running on port {}", port);
 
         let mut net: Network = Network{num_devices: 0,
             interests: interests,
             data: HashMap::new(),
-            broadcast_sock: broadcast_sock,
-            port: tcp_listener.local_addr().unwrap().port()
+            broadcast_sock: None,
+            port: 0,
+            servers_started: false
         };
 
 
-        {
-            let net = &mut net;
+
+
+        net.broadcast_info();
+
+        println!("Servers Started");
+
+        return net;
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // TCP
+    fn start_tcp_serv(network: &mut Network) {
+
+        thread::spawn(|| {
+
+            let mut net = &network;
+
+            let mut core = Core::new().unwrap();
+            let handle = core.handle();
+
+            let tcpaddr = "127.0.0.1:0".parse().unwrap();
+
+            let tcp_listener = match TcpListener::bind(&tcpaddr, &handle) {
+                Ok(lst) => lst,
+                Err(error) => panic!("Couldn't listen for TCP! {}", error)
+            };
+
+
+            net.port = tcp_listener.local_addr().unwrap().port();
+            println!("TCP Server running on port {}", net.port);
 
             let serv = tcp_listener.incoming().for_each(|(sk, peer)|{
                 println!("ASDASD {}", net.port);
                 Ok(())
             });
 
+
             core.run(serv).unwrap();
+        });
+
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    //UDP
+    fn start_udp_serv(net: &mut Network) {
+
+        thread::spawn(|| {
+            let mut core = Core::new().unwrap();
+            let handle = core.handle();
+
+            let udpaddr = "127.0.0.1:52300".parse().unwrap();
+
+            let broadcast_sock: UdpSocket = match UdpSocket::bind(&udpaddr, &handle) {
+                Ok(sock) => sock,
+                Err(error) => panic!("Couldn't listen for UDP! {}", error)
+            };
+
+            broadcast_sock.set_broadcast(true).unwrap();
+
 
             let usrv = UDPServ{net: net};
 
             core.run(usrv).unwrap();
-        }
-
-        net.broadcast_info();
-
-        return net;
+        });
     }
+
 
     pub fn get_num_devices(&self) ->u32 {
         self.num_devices
@@ -89,7 +119,7 @@ impl Network {
 
     fn broadcast_info(&self) {
         let addr = "255.255.255.255:52300".parse().unwrap();
-        match self.broadcast_sock.send_to(protocol::get_broadcast(self.port, &self.interests).as_bytes(), &addr) {
+        match self.broadcast_sock.unwrap().send_to(protocol::get_broadcast(self.port, &self.interests).as_bytes(), &addr) {
             Ok(_) => return,
             Err(error) => println!("Error broadcasting {}", error)
         };
