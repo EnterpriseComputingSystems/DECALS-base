20aaebf70f9231c3bcc49807eab3e9c5a3291c0f
Francesco Macagno <fjmacagno1@yahoo.com>
2018-04-04 11:41:24 -0700

    Added alert enum and helper

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ mod protocol;
 mod device;
 mod data;
 mod event;
+mod support;
 
 use event::Event;
 
---
+++ src/support/mod.rs
@@ -0,0 +1,24 @@
+
+
+mod alert {
+
+    pub enum Alert{
+        Normal,
+        Yellow,
+        Red,
+        Blue,
+        Black,
+
+    }
+
+    pub fn get_alert_text(al: Alert)->String {
+        match al {
+            Alert::Normal => "Normal".to_string(),
+            Alert::Yellow => "Yellow".to_string(),
+            Alert::Red => "Red".to_string(),
+            Alert::Blue => "Blue".to_string(),
+            Alert::Black => "Black".to_string(),
+
+        }
+    }
+}
