89c9da72780499249fdcad37c54dd421538938d7
Francesco Macagno <fjmacagno1@yahoo.com>
2018-04-15 18:41:34 -0700

    added get_alert_from_text

--- src/support/mod.rs
+++ src/support/mod.rs
@@ -1,9 +1,12 @@
 
 
+/// A helper module to provide a common basis for transmitting alert status information.
 pub mod alert {
 
+    /// A common key to be used for use DECALS to synchronize alert status.
     pub const ALERT_KEY: &str = "ALERT_STATUS";
 
+    /// Represents the various possible 'Alert' states a system could be in, a la StarTrek.
     #[derive(Clone, Copy)]
     pub enum Alert{
         Normal,
@@ -14,6 +17,7 @@ pub mod alert {
 
     }
 
+    /// Get a human-readable text representation of an alert
     pub fn get_alert_text(al: Alert)->String {
         match al {
             Alert::Normal => "Normal".to_string(),
@@ -22,6 +26,18 @@ pub mod alert {
             Alert::Blue => "Blue".to_string(),
             Alert::Black => "Black".to_string(),
 
+        }
+    }
+
+    /// Get an alert from a text string. Defaults to 'Normal'
+    pub fn get_alert_from_text(al: String)->Alert {
+        match al.as_str() {
+            "Normal" => Alert::Normal,
+            "Yellow" => Alert::Yellow,
+            "Red" => Alert::Red,
+            "Blue" => Alert::Blue,
+            "Black" => Alert::Black,
+            _ => Alert::Normal
         }
     }
 }
